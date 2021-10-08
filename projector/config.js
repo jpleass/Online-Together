2682c18926e9a99df5f623ea225a729278f7003f
Miksy <mika@cybersuper.space>
2021-10-08 12:40:24 +0200

    hide gui by default

--- projector/config.js
+++ projector/config.js
@@ -2,7 +2,12 @@ import { GUI } from 'dat.gui';
 import refreshPage from "./projector.js";
 
 
-const gui = new GUI();
+const gui = new GUI({
+  autoPlace: true, // this is default
+  hideable: true, // this is default, press 'h' to show/hide
+});
+
+gui.hide()
 
 const blend_modes = ["source-over","source-in","source-out","source-atop","destination-over","destination-in","destination-out","destination-atop","lighter","copy","xor","multiply","screen","overlay","darken","lighten","color-dodge","color-burn","hard-light","soft-light","difference","exclusion","hue","saturation","color","luminosity",];
 
@@ -40,16 +45,5 @@ gui.add( config, 'blur_iterations', 1, 50, 1 );
 
 var resetButton = { reset: () => { refreshPage(); } };
 gui.add(resetButton,'reset');
-
-document.addEventListener('keypress', toggleStats);
-
-let hideStats = false;
-function toggleStats(e) {
-  if (e === '72') {
-    hideStats
-      ? document.querySelector('.dg').style.display = 'none'
-      : document.querySelector('.dg').style.display = 'block'
-  }
-}
 
 export default config;
